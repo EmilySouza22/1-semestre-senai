43e8c386653c26fc263153714a68bbf7205bf8ab
EmilySouza22 <emilyseafonso@gmail.com>
2025-04-09 11:58:52 -0300

    Adiciona exercício20 js

---
+++ Javascript/Javascript-inicial/Ex20/script.js
@@ -0,0 +1,17 @@
+/*
+20. Você está tentando se comunicar com um alien usando sinais booleanos.
+
+let sinal1 = true;
+let sinal2 = false;
+
+Se ambos forem true , envie "Sinal de paz enviado 🛸", senão "Sem contato estabelecido."
+*/
+
+let sinal1 = true;
+let sinal2 = false;
+
+if(sinal1 == true && sinal2 == true){
+    console.log("Sinal de paz enviado 🛸")
+}else{
+    console.log("Sem contato estabelecido.")
+}
