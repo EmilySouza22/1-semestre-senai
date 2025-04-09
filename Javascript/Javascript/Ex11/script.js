3f63910a54fc5324f4144a6e6482b742e8b9da91
EmilySouza22 <emilyseafonso@gmail.com>
2025-04-09 02:02:37 -0300

    Adiciona exercício11 js

---
+++ Javascript/Javascript/Ex11/script.js
@@ -0,0 +1,19 @@
+/*
+11. A entrada no cinema é gratuita 
+para crianças até 12 anos ou idosos acima de 60.
+
+Desafio:
+let idade;
+
+Exiba:
+"Entrada gratuita 🎟" ou
+"Precisa pagar ingresso 💳"
+*/
+
+let idade = prompt("Digite sua idade:")
+
+if(idade <= 12 || idade > 60){
+    console.log("Entrada gratuita 🎟")
+} else {
+    console.log("Precisa pagar ingresso 💳")
+}
