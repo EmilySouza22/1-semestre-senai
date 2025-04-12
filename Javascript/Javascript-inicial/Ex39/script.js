1a880ac783080cd171d38c21c3196b5564241960
EmilySouza22 <emilyseafonso@gmail.com>
2025-04-11 22:57:18 -0300

    Adiciona Ex39 js

---
+++ Javascript/Javascript-inicial/Ex39/script.js
@@ -0,0 +1,12 @@
+/*
+39. Se a temperatura estiver abaixo de 0, diga "Água congelando!"
+
+let temperatura;
+*/
+let temperatura = prompt("A temperatura está abaixo de 0?");
+
+if(temperatura == 'sim'){
+    console.log("Água congelando!");
+}else{
+    console.log("Tudo normal...");
+}
