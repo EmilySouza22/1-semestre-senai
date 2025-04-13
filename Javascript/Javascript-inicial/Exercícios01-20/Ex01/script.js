4edf991d1cda8d1a9c48b90e1dc93cdff376b408
EmilySouza22 <emilyseafonso@gmail.com>
2025-04-12 23:35:23 -0300

    Organiza os exercícios nas pastas

---
+++ Javascript/Javascript-inicial/Exercícios01-20/Ex01/script.js
@@ -0,0 +1,17 @@
+/*
+1. Dado o código abaixo, use typeof para identificar os tipos de cada variável e
+imprima a frase correta.
+let feitiço = "Lumos";
+let nívelDeMana = 87;
+let invisível = false;
+console.log(`O feitiço é do tipo: ???`);
+*/
+
+let feitiço = "Lumos";
+let nivelDeMana = 87;
+let invisivel = false;
+
+
+console.log(`O feitiço é do tipo: ` + typeof(feitiço));
+console.log(`O feitiço é do tipo: ` + typeof(nivelDeMana));
+console.log(`O feitiço é do tipo: ` + typeof(invisivel));
