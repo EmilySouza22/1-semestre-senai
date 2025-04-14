2db94c3af8c299b8a612400e105d96098d3c0504
EmilySouza22 <emilyseafonso@gmail.com>
2025-04-14 01:00:57 -0300

    Adiciona exemplo function

--- Javascript/Javascript-inicial/Exemplos/exemplo-função/script.js
+++ Javascript/Javascript-inicial/Exemplos/exemplo-função/script.js
@@ -5,17 +5,24 @@ function olaMundo(){
     console.log("Olá mundo novamente")
     console.log("Olá mundo mais uma vez...")
 }
-olaMundo();
 
 function somar(valor1, valor2){
     let resultado = valor1 + valor2;
-    console.log(resultado);
+    console.log(resultado)
 }
-somar(1,2);
-somar(4,4);
-somar(99,1);
 
 let subtrair = function(valor3,valor4){
     console.log(valor3 - valor4);
     return valor3 - valor4;
 }
+
+function possuiCNH (valorString) {
+    console.log('AI MEUDEUS', valorString)
+}
+
+const pessoa = {
+    nome: "Emily",
+    idade: 20,
+    cnh: possuiCNH("SIM")
+}
+console.log(pessoa)
