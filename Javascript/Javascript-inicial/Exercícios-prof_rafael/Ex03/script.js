fc8a30e0d1dde40849dfc847cc1230e09fec5497
Emily <emilyseafonso@gmail.com>
2025-04-17 22:01:15 -0300

    Add ex03

--- Javascript/Javascript-inicial/Exercícios-prof_rafael/Ex03/script.js
+++ Javascript/Javascript-inicial/Exercícios-prof_rafael/Ex03/script.js
@@ -1,3 +1,16 @@
 /*
-3. 
+1 ETAPA
+Ler a distancia da próxima corrida e dizer quanto tempo vai levar.
+1 dado de entrada: distancia (em km).
+1 dado de saída: tempo de viagem (em segs).
+*/ 
+
+/*
+2 ETAPA
+O tempo de viagem já é apresentado em segundos...
+Caso este tempo seja maior que um minuto, apresentar este valor também em minutos.
+Caso seja maior que uma hora, apresentar também em horas;
+Caso seja maior que um dia, apresentar também em dias;
+Caso seja maior que 30 dias, apresentar também em meses
+Caso seja maior que 12 meses, apresentar também em anos
 */
