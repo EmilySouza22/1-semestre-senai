1b2d87c7f116f948ed642781832cf596706062ea
EmilySouza22 <emilyseafonso@gmail.com>
2025-04-23 15:23:02 -0300

    Adiciona atualização css

--- Javascript/Javascript-inicial/Desafios/desafio02/script.js
+++ Javascript/Javascript-inicial/Desafios/desafio02/script.js
@@ -13,3 +13,8 @@ function exercicio002(){
     let media = (num1+num2)/2
     alert("O resultado da média entre " + num1 + " + " + num2 + " será " + media);   
 }
+
+const screenWidth = window.screen.width;
+const screenHeight = window.screen.height;
+
+console.log(`Largura: ${screenWidth}px, Altura: ${screenHeight}px`);
