ab9f235cf1e589fe9e91419cb92c904cb9daf1e8
Emily <emilyseafonso@gmail.com>
2025-04-23 14:38:30 -0300

    Add arq .js

--- Javascript/Javascript-inicial/Desafios/desafio02/script.js
+++ Javascript/Javascript-inicial/Desafios/desafio02/script.js
@@ -1,2 +1,15 @@
-/*
-*/
+function exercicio001(){
+    let num1 = Number(prompt("Digite o primeiro número: "));
+    let num2 = Number(prompt("Digite o segundo número: "));
+
+    let soma = num1 + num2;
+    alert("O resultado da soma entre " + num1 + " + " + num2 + " será " + soma);
+}
+
+function exercicio002(){
+    let num1 = Number(prompt("Digite o primeiro número: "));
+    let num2 = Number(prompt("Digite o segundo número: "));
+    
+    let media = (num1+num2)/2
+    alert("O resultado da média entre " + num1 + " + " + num2 + " será " + media);   
+}
