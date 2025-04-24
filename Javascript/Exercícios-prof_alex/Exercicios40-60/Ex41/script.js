2e4023f490a6d72162f54e460ca549f218a511be
Emily <emilyseafonso@gmail.com>
2025-04-23 21:28:07 -0300

    Adiciona exercicio41

---
+++ Javascript/Exercícios-prof_alex/Exercicios40-60/Ex41/script.js
@@ -0,0 +1,9 @@
+/*
+Ex41. Saudação Personalizada
+Solicite ao usuário que digite seu nome usando prompt .
+Exiba uma saudação personalizada no formato: "Olá, [nome]! Seja bemvindo!" usando alert .
+*/
+
+let nomeUsuario = prompt("Qual é o seu nome?");
+
+alert("Olá, " + nomeUsuario + "! Seja bem vindo!")
