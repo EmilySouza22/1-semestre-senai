f87458d356e703bd4355c39db87b3161b68345af
Emily <emilyseafonso@gmail.com>
2025-04-30 17:07:53 -0300

    Adiciona exercíciosJS

---
+++ Javascript/Exercícios-prof_rafael/ExercíciosJS/script.js
@@ -0,0 +1,77 @@
+function exercicio1(){
+    //Quanto você ganha por dia trabalhado?
+    let salario = Number(prompt("Digite quanto você ganha no mês."));
+    let dias = 30;
+
+    let salario_diario = salario / dias;
+
+    alert(`Você ganha por dia ${salario_diario}`);
+}
+
+function exercicio2(){
+    let valorCafe = Number(prompt("Digite o valor do café: "));
+    let valorCoxinha = Number(prompt("Digite o valor da coxinha: "));
+    let valorHalls = Number(prompt("Digite o valor do halls: "));
+    let valorDoceDeLeite = Number(prompt("Digite o valor do doce de leite: "));
+
+    let conta = valorCafe + valorCoxinha + valorDoceDeLeite + valorHalls
+
+    alert(`O valor a pagar na cantina será R$ ${conta}`)
+}
+
+function exercicio3(){
+    let nota1 = Number(prompt("Digite sua primeira nota: "));
+    let nota2 = Number(prompt("Digite sua segunda nota: "));
+    let nota3 = Number(prompt("Digite sua terceira nota: "));
+
+    media = nota1 + nota2 + nota3
+
+    alert(`A média será: ${media}`)
+}
+
+function exercicio4(){
+    let vitorias = Number(prompt("Insira o número de vitórias."));
+    let empates = Number(prompt("Insira o número de vitórias."));
+
+    let pontos = vitorias * 3 + empates
+    alert(`A pontuação do time será: ${pontos} pontos.`)
+}
+
+function exercicio5(){
+    
+}
+
+function exercicio6(){
+    
+}
+
+function exercicio7(){
+    
+}
+
+function exercicio8(){
+    
+}
+
+function exercicio9(){
+    
+}
+
+function exercicio10(){
+    
+}
+
+function exercicio11(){
+    
+}
+
+function exercicio12(){
+    
+}
+
+function exercicio13(){
+    
+}
+function exercicio14(){
+    
+}
