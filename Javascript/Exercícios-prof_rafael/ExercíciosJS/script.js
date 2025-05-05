5a776c2d7618154248711070e09d4d23bd200764
EmilySouza22 <emilyseafonso@gmail.com>
2025-05-05 15:17:23 -0300

    Adicioona comentários

--- Javascript/Exercícios-prof_rafael/ExercíciosJS/script.js
+++ Javascript/Exercícios-prof_rafael/ExercíciosJS/script.js
@@ -1,5 +1,10 @@
-function exercicio1(){
-    //Quanto você ganha por dia trabalhado?
+/**
+ * Exercícios tirados de /github.com/rafaellindemann/materiaisDeAula-permanente
+ * 2 Operadores matemáticos com entrada de dados 
+ */
+
+
+function exercicio1(){ //Quanto você ganha por dia trabalhado?
     let salario = Number(prompt("Digite quanto você ganha no mês."));
     let dias = 30;
 
@@ -8,60 +13,64 @@ function exercicio1(){
     alert(`Você ganha por dia ${salario_diario}`);
 }
 
-function exercicio2(){
+function exercicio2(){ //Conta da cantina
     let valorCafe = Number(prompt("Digite o valor do café: "));
     let valorCoxinha = Number(prompt("Digite o valor da coxinha: "));
     let valorHalls = Number(prompt("Digite o valor do halls: "));
     let valorDoceDeLeite = Number(prompt("Digite o valor do doce de leite: "));
 
-    let conta = valorCafe + valorCoxinha + valorDoceDeLeite + valorHalls
+    let conta = valorCafe + valorCoxinha + valorDoceDeLeite + valorHalls;
 
-    alert(`O valor a pagar na cantina será R$ ${conta}`)
+    alert(`O valor a pagar na cantina será R$ ${conta}`);
 }
 
-function exercicio3(){
+function exercicio3(){ //Média 3 notas
     let nota1 = Number(prompt("Digite sua primeira nota: "));
     let nota2 = Number(prompt("Digite sua segunda nota: "));
     let nota3 = Number(prompt("Digite sua terceira nota: "));
 
-    media = nota1 + nota2 + nota3
+    media = nota1 + nota2 + nota3;
 
-    alert(`A média será: ${media}`)
+    alert(`A média será: ${media}`);
 }
 
-function exercicio4(){
+function exercicio4(){ //Pontuação time
     let vitorias = Number(prompt("Insira o número de vitórias."));
     let empates = Number(prompt("Insira o número de vitórias."));
 
-    let pontos = vitorias * 3 + empates
-    alert(`A pontuação do time será: ${pontos} pontos.`)
+    let pontos = vitorias * 3 + empates;
+    alert(`A pontuação do time será: ${pontos} pontos.`);
 }
 
-function exercicio5(){
+function exercicio5(){ //Cálculo do dano crítico em um jogo
+
+}
+
+function exercicio6(){ //Cálculo do dano crítico em um jogo + bonus
     
 }
 
-function exercicio6(){
+function exercicio7(){ //Média ponderada de duas notas
     
 }
 
-function exercicio7(){
+function exercicio8(){ //Poupança
     
 }
 
-function exercicio8(){
+function exercicio9(){ //Café por alunos
     
 }
 
-function exercicio9(){
+function exercicio10(){ //Café por alunos + extra
     
 }
 
-function exercicio10(){
-    
-}
+/**
+ * 3 - Criação de programas com operadores matemáticos [3.1 até 3.17]
+ */
 
-function exercicio11(){
+function exercicio11(){ 
     
 }
 
