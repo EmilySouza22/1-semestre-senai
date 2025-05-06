f326939a0491616024a5d7199c0aafa4ac89f3f6
Emily <emilyseafonso@gmail.com>
2025-05-05 21:28:08 -0300

    Adiciona 3 exercícios sobre While

---
+++ Javascript/Exercícios-prof_alex/ExercíciosWhile2/script.js
@@ -0,0 +1,44 @@
+function parImpar(){
+    let num = 1;
+    let max = 10;
+    while(num <= max){
+        num % 2 === 0 ? alert(`${num}: Par`) : alert(`${num}: Ímpar`);
+        num++;
+    }
+}
+
+function tabuadas(){
+    let contador = 1;
+    let max = 10;
+    while(contador <= max){
+        let contador2 = 1;
+        while(contador2 <= max){
+            console.log(`${contador} x ${contador2} = ${contador * contador2}`);
+            contador2++;
+        }
+        contador++;
+    }
+}
+
+function senha(){
+    const senhaCorreta = "4321";
+    const min = 0;
+    let acertou = false;
+
+    let index = 3;
+    while(index >= min && acertou === false){
+        let senha = prompt(`Digite a senha:`);
+        if(senha == senhaCorreta){
+            alert(`Senha correta!`);
+            acertou = true;
+        }else{
+            alert(`Senha incorreta! Você tem mais ${index} tentativas`);
+        }
+        index--;
+    }
+    if(acertou === true){
+        alert(`Você logou na conta`);
+    }else{
+        alert(`Você errou sua senha muitas vezes. Sua conta foi bloqueada`);
+    }
+}
