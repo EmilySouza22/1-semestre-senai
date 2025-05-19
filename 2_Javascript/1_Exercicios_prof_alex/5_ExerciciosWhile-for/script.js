6f6721d68b3586b500a1d96adef3ad741c2e5db7
Emily <emilyseafonso@gmail.com>
2025-05-19 19:46:41 -0300

    Adiciona exercicio1 js

--- 2_Javascript/1_Exercicios_prof_alex/5_ExerciciosWhile-for/script.js
+++ 2_Javascript/1_Exercicios_prof_alex/5_ExerciciosWhile-for/script.js
@@ -1,13 +1,31 @@
 function exercicio1(){
+    /* 1. Peça ao usuário sua idade e se ele tem carteira de motorista. Exiba "Pode
+    dirigir" se for maior de 18 e tiver carteira. */
 
+    let idadeUsuario = parseInt(prompt(`Insira sua idade`));
+    let temCarteira = prompt(`Você tem carteira de motorista? (sim ou nao)`);
+
+    if(temCarteira == 'sim'){
+        temCarteira = true;
+    }else{
+        temCarteira = false;
+    }
+
+    if(temCarteira === true && idadeUsuario >= 18){
+        alert(`Pode dirigir`);
+    }else{
+        alert(`Não pode dirigir`);
+    }
 }
 
 function exercicio2(){
-    
+    /* 2. Verifique se um número digitado é positivo e par. */
+
 }
 
 function exercicio3(){
-    
+    /* 3. Solicite um nome de usuário e uma senha. Exiba "Acesso permitido" se o
+    nome for "admin" ou a senha for "1234". */
 }
 
 function exercicio4(){
