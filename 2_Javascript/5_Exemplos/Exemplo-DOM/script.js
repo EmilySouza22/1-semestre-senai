8f1e1813fcefc326d9370da19af33df65db83121
Emily <emilyseafonso@gmail.com>
2025-05-20 19:27:23 -0300

    Add exemple DOM

---
+++ 2_Javascript/5_Exemplos/Exemplo-DOM/script.js
@@ -0,0 +1,9 @@
+function soma(){
+    let n1 = parseInt(document.getElementById("n1").value);
+    let n2 = parseInt(document.getElementById("n2").value);
+
+    let p = document.getElementById("resultado");
+
+    resultado = n1 + n2;
+    p.innerText = "Resultado: " + resultado;
+}
