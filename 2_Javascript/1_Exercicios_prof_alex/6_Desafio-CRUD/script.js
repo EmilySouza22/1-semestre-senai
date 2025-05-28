5cf496e9082c2eadaf6d2be8bc28194fbbea7b68
EmilySouza22 <emilyseafonso@gmail.com>
2025-05-27 21:50:29 -0300

    Implementa função de adicionar item a lista

--- 2_Javascript/1_Exercicios_prof_alex/6_Desafio-CRUD/script.js
+++ 2_Javascript/1_Exercicios_prof_alex/6_Desafio-CRUD/script.js
@@ -2,3 +2,18 @@ function chamarCadastro(){
     document.getElementById("container_cadastro").style.display = "flex"
     document.getElementById("container_principal").style.display = "none"
 }
+
+function chamarVoltar(){
+    document.getElementById("container_cadastro").style.display = "none"
+    document.getElementById("container_principal").style.display = "flex"
+}
+
+function adicionarItem(){
+    let itemAdicionar = document.getElementById("nome").value;
+    let lista = document.getElementById("minhaLista");
+    let li = document.createElement("li");
+    let conteudo = `<li> ${itemAdicionar} <img src="img/pencil.svg" alt="ícone de pincel" class="editar"> <img src="img/trash.svg" alt="ícone de lixeira" class="deletar"> </li>`
+    li.innerHTML = conteudo;
+    lista.appendChild(li);
+}
+
