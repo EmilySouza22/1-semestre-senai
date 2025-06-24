f58aa8085d2232e87d8a9d1c1145c3a783793769
EmilySouza22 <emilyseafonso@gmail.com>
2025-06-24 14:57:03 -0300

    Adiciona exercício 14 arrays

--- 2_Javascript/1_Exercicios_prof_alex/7_Exercicios-Array/script.js
+++ 2_Javascript/1_Exercicios_prof_alex/7_Exercicios-Array/script.js
@@ -1,61 +1,59 @@
 //11. Crie um array com 5 nomes e mostre apenas os que têm mais de 4 letras.
-
 const alunos = ['ana', 'gabriela', 'mary', 'emily'];
 const filtroNome = alunos.filter(nomesMaioresQue4Letras);
 function nomesMaioresQue4Letras(nomeAluno){
     return nomeAluno.length > 4;
 }
-console.log(filtroNome)
+console.log('Exercício 11:', filtroNome)
 
 //12. Dobre cada número de um array usando .map()
-
 const numeros = [1,2,3,4,100];
 const numerosDobro = numeros.map(numero => numero * 2);
-console.log(numerosDobro);
-
+console.log('Exercício 12:', numerosDobro);
 
 //13. Filtre os números pares de um array com .filter().
-
 const numbers = [1,2,3,5,7,12,14,15,17,33,34,56,89,90];
 const filterOdd = numbers.filter(numbersOdd);
 function numbersOdd(numbersodd){
     return numbersodd % 2 === 0;
 }
-console.log(filterOdd)
+console.log('Exercício 13:', filterOdd)
 
+//14. Some todos os números do array com .reduce().
+const numerosSomados = numeros.reduce((acumulador, valorAtual) => {
+    return acumulador + valorAtual
+});
+console.log('Exercício 14:', numerosSomados)
 
 
 // 27. coloque a primeira letra de cada palavra em maiuscula
-
 let palavras2 = ['teste', 'seila', 'qualquer', 'coisa']
 let palavrasMaiusculo = palavras2.map((palavra)=> {
     let [primeira, ...restante] = palavra
     let novaletra = palavra[0].toUpperCase()
     return novaletra + restante.join('')
 })
-console.log(palavrasMaiusculo)
+console.log('Exercício 27:', palavrasMaiusculo)
 
 //28. Verifique se todos os elementos são números com .every().
-
 let elementos = [3,"4",5,6,8];
 let saonumeros = elementos.every(elemento => typeof elemento === 'number')
-console.log(saonumeros);
+console.log('Exercício 28:', saonumeros);
 
 //29. Verifique se ao menos um npumero é maior que 100 usando .some().
 let elementos2 = [33,45,500,6,7,8]
 let teste = elementos2.some(elemento => elemento > 100)
-console.log(teste)
+console.log('Exercício 29:', teste)
 
 //30. "Achatamento": transforme um arrau com subarraus em um único array plano (um nível só)
 let arraydearray = [34,21,[32,21,58],[2,4,7]]
 let arrays = arraydearray.flat();
-console.log(arrays);
+console.log('Exercício 30:', arrays);
 
 // 32.Crie uma função que recebe um arrau de números e retorna a média
-
 let notas = [5,6,7,8,10]
 function calcularMedia(array){
     let soma = array.reduce((acumulador,atual)=> acumulador += atual ,0)
     return soma/array.length
 }
-console.log(calcularMedia(notas));
+console.log('Exercício 32:', calcularMedia(notas));
