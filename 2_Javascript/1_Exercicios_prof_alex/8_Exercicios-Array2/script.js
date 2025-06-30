53db5627a292cd07b9a06ba2cb1ed373b6fe8570
Emily <emilyseafonso@gmail.com>
2025-06-29 21:35:05 -0300

    Adiciona exercícios array numeros

--- 2_Javascript/1_Exercicios_prof_alex/8_Exercicios-Array2/script.js
+++ 2_Javascript/1_Exercicios_prof_alex/8_Exercicios-Array2/script.js
@@ -36,3 +36,29 @@ const menor = numeros.reduce((menorAtual , numero) => {
     return numero < menorAtual ? numero : menorAtual
 }, numeros[0])
 console.log(`Exercício 5: ${menor}`);
+
+//6. Ordenar os números em ordem crescente
+const numerosOrdemCrescente = numeros.sort((a,b) => a - b);
+console.log(`Exercício 6: ${numerosOrdemCrescente}`);
+
+//7. Criar um novo array com apenas os números ímpares
+const numerosImpares = numeros.filter(numero => numero % 2 != 0);
+console.log(`Exercício 7: ${numerosImpares}`);
+
+//8. Verificar se todos os números são maiores que 3
+let maiorQue3 = true;
+const verificacao = numeros.map(numero => {
+    if(numero <= 3){
+        maiorQue3 = false;
+    }
+});
+console.log(`Exercício 8: TODOS os números são maiores que 3? Resposta: ${maiorQue3}`);
+
+//9. Verificar se existe algum número maior que 100.
+let numeroMaior = false;
+const verificacao2 = numeros.find(numero => numero > 100 ? numeroMaior = true : numeroMaior = false);
+console.log(`Exercício 9: Há algum número maior que 100? Resposta: ${numeroMaior}`);
+
+//10. Retornar a média dos números.
+const media = numeros.reduce((soma, numero) => soma + numero, 0) / numeros.length;
+console.log(`Exercício 10: ${media.toFixed(2)}`);
